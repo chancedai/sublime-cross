2a1c17fbaefb0b9b3c96ec0457b1a45552f01b75
chancedai <340433246@qq.com>
2013-01-15 23:37:08 +0800

    create a cursor-centered cross, highling current line, and create a ruler  a verticle line
    Signed-off-by: chancedai <340433246@qq.com>

---
+++ cross.py
@@ -0,0 +1,51 @@
+"""
+	OPTIONS
+	"show_cross": true
+		show cross.
+
+"""
+import sublime
+import sublime_plugin
+settings_cross = sublime.load_settings('Cross.sublime-settings')
+class CrossCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		show_cross = int(bool(settings_cross.get('show_cross')))
+		settings_cross.set('show_cross',not show_cross)
+
+def unload_handler():
+	for window in sublime.windows():
+		for view in window.views():
+			view.erase_regions('CrossListener')
+class CrossListener(sublime_plugin.EventListener):
+	def __init__(self):
+		print '---------------------------------- cross init begin'
+		# settings_cross.add_on_change('reload', lambda: self.update_cross(sublime.active_window().active_view()))
+		print sublime.active_window()
+		# self.update_cross(sublime.active_window().active_view())
+		print '---------------------------------- cross init end'
+	def update_cross(self, view):
+		print 666
+		settings_index = view.settings()
+		show_cross = int(bool(settings_cross.get('show_cross')))
+		show_multiple_cross = int(bool(settings_cross.get('show_multiple_cross')))
+		if not show_cross:
+			settings_index.set('rulers',[])
+			return
+		rulers = []
+		for sel in reversed(view.sel()):
+			end_sel = sel.end()
+			pos_xy = view.text_to_layout(end_sel)
+			pos_x = pos_xy[0]
+			pos = int(pos_x/view.em_width())
+			rulers.append(pos)
+			if not show_multiple_cross:
+				break
+		settings_index.set('rulers',rulers)
+	def on_load(self, view):
+		self.update_cross(view)
+	def on_activated(self, view):
+		self.update_cross(view)
+	def on_modified(self, view):
+		self.update_cross(view)
+	def on_selection_modified(self, view):
+		self.update_cross(view)
