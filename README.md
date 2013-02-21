a68de8c9e3afc091960816ac36ed685ea5282b3b
chancedai <340433246@qq.com>
2013-02-22 00:52:13 +0800

    Show multiple vertical line while you have multiple selections
    Signed-off-by: chancedai <340433246@qq.com>

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
 sublime-cross
 =============
 
-Create a  cursor-centered cross
+Create a cursor-centered cross
 
 
-## Install
+## Installation
 
 1. Clone or [download](/chancedai/sublime-cross/archive/master.zip) git repo into your packages folder (in ST2, find Browse Packages... menu item to open this folder)
 2. Restart ST2 editor (if required)
 
-Or with [Package Control](http://wbond.net/sublime_packages/package_control):
-1. Run “Package Control: Install Package” command, find and install `Cross` plugin.
-2. Restart ST2 editor (if required)
+## Features
+
+1. Customize width of vertical line
+2. Show multiple vertical line while you have multiple selections
+
+## Configuration ##
+
+These are the settings that I prefer.You can change the value of the options like so:
+
+```json
+{
+	"show_cross" : true,
+	"show_multiple_cross":true,
+	"cross_width":4
+}
+```
--- cross.py
+++ cross.py
@@ -6,6 +6,7 @@
 		show multiple cross
 
 """
+import math
 import sublime
 import sublime_plugin
 settings_cross = sublime.load_settings('Cross.sublime-settings')
@@ -20,21 +21,33 @@ def unload_handler():
 			view.erase_regions('CrossListener')
 class CrossListener(sublime_plugin.EventListener):
 	def __init__(self):
-		print '---------------------------------- cross init'
+		print '---------------------------------- 20130222 cross init'
 	def update_cross(self, view):
 		settings_index = view.settings()
+		viewport = view.viewport_extent()
+		if viewport[1]<30:
+			settings_index.set('rulers','')
+			return
 		show_cross = int(bool(settings_cross.get('show_cross')))
 		show_multiple_cross = int(bool(settings_cross.get('show_multiple_cross')))
+		cross_width = int(settings_cross.get('cross_width'))
 		if not show_cross:
 			settings_index.set('rulers',[])
 			return
 		rulers = []
+		gap = 0.5/view.em_width()
+		start = -math.ceil(cross_width)*gap
+
+
 		for sel in reversed(view.sel()):
 			end_sel = sel.end()
 			pos_xy = view.text_to_layout(end_sel)
 			pos_x = pos_xy[0]
 			pos = int(pos_x/view.em_width())
 			rulers.append(pos)
+			for i in range(0,cross_width*2):
+				rulers.append(pos+start)
+				start+=gap
 			if not show_multiple_cross:
 				break
 		settings_index.set('rulers',rulers)
